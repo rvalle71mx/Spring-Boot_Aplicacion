8a3c4db71b216cb80161c60bcd4e57a57a1850bc
rvalle71mx <33527447+rvalle71mx@users.noreply.github.com>
2020-03-23 19:00:35 -0600

    Commit Inicial con Eclipse

---
+++ SpringBootAplicacion/src/test/java/mx/com/cibercafe/SpringBootAplicacion/SpringBootAplicacionApplicationTests.java
@@ -0,0 +1,16 @@
+package mx.com.cibercafe.SpringBootAplicacion;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootAplicacionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
