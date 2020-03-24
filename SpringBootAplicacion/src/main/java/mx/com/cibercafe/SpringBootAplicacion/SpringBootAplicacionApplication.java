e8cc23c5961be6debef3b37f1cdc506d933806ec
rvalle71mx <33527447+rvalle71mx@users.noreply.github.com>
2020-03-23 19:00:35 -0600

    Commit Inicial con Eclipse. Close #1

---
+++ SpringBootAplicacion/src/main/java/mx/com/cibercafe/SpringBootAplicacion/SpringBootAplicacionApplication.java
@@ -0,0 +1,13 @@
+package mx.com.cibercafe.SpringBootAplicacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAplicacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAplicacionApplication.class, args);
+	}
+
+}
